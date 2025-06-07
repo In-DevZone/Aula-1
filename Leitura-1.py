6170e91d321c7184428158c41a57583892a27fc0
In-DevZone <nany.moraes2007@gmail.com>
2025-06-07 14:31:38 -0300

    Add files via upload

---
+++ Leitura-1.py
@@ -0,0 +1,12 @@
+def main():
+  nomeEscola = input("Digite o nome da escola:  ")
+  idade = int(input("Digite a idade:  "))
+  altura = float(input("Digite a altura:  "))
+  sexo = input("Digite o sexo:  ")
+
+  print(nomeEscola)
+  print(idade)
+  print(altura)
+  print(sexo)
+  return 0
+main()
